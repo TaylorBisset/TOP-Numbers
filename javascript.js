b1a943bb12c708abfe067afdd67ad1bd64824c8d
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 13:02:45 -0700

    percentage test

--- javascript.js
+++ javascript.js
@@ -8,3 +8,8 @@ let b = 7 * a;
 console.log(a);
 console.log(9 * a);
 console.log(b);
+
+let MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+// percentage should read a value of 0.7719 in the console. 
